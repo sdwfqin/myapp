5d1d90f97648f67c12590437c5500b41ee6030a2
zhangqin <sdwfqin@gmail.com>
2019-12-25 16:50:18 +0800

    :sparkles: 角色列表

--- src/pages/system/role/index.jsx
+++ src/pages/system/role/index.jsx
@@ -1,10 +1,16 @@
-import {Table} from 'antd';
+import {Button, Card, Col, Row, Table} from 'antd';
 import React, {Component} from 'react';
 import {connect} from 'dva';
+import {PageHeaderWrapper} from "@ant-design/pro-layout";
+import styles from '@/utils/utils.less';
 
 class Role extends Component {
 
   state = {
+    pagination: {
+      current: 1,
+      defaultPageSize: 10
+    },
     roleTitles: [
       {
         title: '角色',
@@ -42,30 +48,66 @@ class Role extends Component {
   };
 
   componentDidMount() {
-    this.handleTableDatas();
+    const {pagination} = this.state;
+
+    this.handleTableDatas({
+      limit: pagination.current,
+      offset: pagination.defaultPageSize
+    });
   }
 
-  handleTableDatas = () => {
+  /**
+   * 加载数据
+   * @param payload
+   */
+  handleTableDatas = payload => {
     const {dispatch} = this.props;
+
     dispatch({
       type: 'role/fetchRoleList',
-      payload: {},
+      payload,
+    });
+  };
+
+  handleTableChange = (pagination, filters, sorter) => {
+    const pager = {...this.state.pagination};
+    pager.current = pagination.current;
+    this.setState({
+      pagination: pager,
+    });
+
+    this.handleTableDatas({
+      limit: pager.current,
+      offset: pager.defaultPageSize
     });
   };
 
   render() {
     const {role = {}, roleListing} = this.props;
     const {roleDataSource} = role;
-    const {roleTitles} = this.state;
+    const {roleTitles, pagination} = this.state;
     return (
-      <div>
-        <Table
-          bordered
-          rowKey={record => record.id}
-          loading={roleListing}
-          dataSource={roleDataSource}
-          columns={roleTitles}/>
-      </div>
+      <PageHeaderWrapper>
+        <Card>
+          <Row gutter={16} type="flex" justify="space-between" align="middle"
+               className={styles.tableHeadRow}>
+            <Col span={12} align="left">
+              <Button type="primary">添加</Button>
+            </Col>
+            <Col span={12} align="right">
+
+            </Col>
+          </Row>
+          <Table
+            bordered
+            rowKey={record => record.id}
+            loading={roleListing}
+            dataSource={roleDataSource}
+            columns={roleTitles}
+            pagination={pagination}
+            onChange={this.handleTableChange}/>
+        </Card>
+      </PageHeaderWrapper>
     );
   }
 }
